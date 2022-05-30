e203ec62d991da2ca299e4752b53ce2e84548512
Shushu9 <pokotilo.gleb1@gmail.com>
2022-05-30 21:13:04 +0300

    finished modal and pageup

--- js/script.js
+++ js/script.js
@@ -17,6 +17,9 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 $(document).ready(function(){
+
+    //Carousel
+
     $('.feed__carousel').slick({
         speed: 1200,
         // infinite: true,
@@ -38,5 +41,46 @@ $(document).ready(function(){
                 }
             }
         ]    
-    });     
+    });
+    
+    // Smooth scroll and pageup
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 800) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+            // Store hash
+            var hash = this.hash;
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+
+
+    //modal
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation').fadeOut('slow');
+    });
+
 });
